688e08ee2149f7067586e2a48521077d03f78182
Andrew Tuley <drewtuley@gmail.com>
2017-03-16 14:16:41 +0000

    add stats output

--- SqliteTest.py
+++ SqliteTest.py
@@ -5,9 +5,9 @@ from datetime import datetime
 from random import randint
 
 if __name__ == "__main__":
-    insert_count = 1e5
+    insert_limit = 1e5
     if len(sys.argv) > 1:
-        insert_count = int(sys.argv[1])
+        insert_limit = int(sys.argv[1])
 
     db_filename = "data/sqlite_test.db"
     schema_filename = "data/sqlite3_test.sql"
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         inserts = 0
         conflicts = 0
         upper = 256 ** 3
-        while inserts < insert_count:
+        while inserts < insert_limit:
             sql = 'insert into registration select \"{0}\",\"REG\",\"{1}\"'.format('{:06X}'.format(randint(1, upper)),
                                                                                    str(datetime.now()))
             try:
@@ -37,6 +37,19 @@ if __name__ == "__main__":
             except sqlite3.IntegrityError:
                 conflicts += 1
     try:
-        print('Runtime = {0} with {1} conflicts'.format(datetime.now() - start, conflicts))
+        print('Inserted {0} rows in {1} with {2} conflicts'.format(int(insert_limit), datetime.now() - start, conflicts))
+        
+        analysis_sql = 'select strftime(\'%s\',created), count(*) from registration group by strftime(\'%s\', created);'
+        cursor = conn.cursor()
+        cursor.execute(analysis_sql)
+    
+        time_base = -1
+        print('time,rows')
+        for row in cursor.fetchall():
+            if time_base == -1:
+                time_base = int(row[0])
+            
+            print('{0},{1}'.format(int(row[0])-time_base, row[1]))
+
     except NameError:
         print('False start')
